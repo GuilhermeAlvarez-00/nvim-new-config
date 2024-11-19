0e19f421de9504bcad6ed037e86de641b9663562
Guilherme Alvarez <guilherme.barboza@veredaeducacao.com>
2024-11-19 09:18:20 -0300

    refactor: add config to tmux and to autoclose

--- init.lua
+++ init.lua
@@ -2,3 +2,19 @@ require("config.options")
 require("config.lazy")
 require("config.keymaps")
 
+-- TMUX COLORSCHEME CONFIG
+vim.cmd("colorscheme fluoromachine")  -- Replace with your colorscheme
+vim.cmd([[
+    highlight Normal guibg=NONE ctermbg=NONE
+    highlight LineNr guibg=NONE ctermbg=NONE
+    highlight SignColumn guibg=NONE ctermbg=NONE
+    highlight NormalNC guibg=NONE ctermbg=NONE
+]])
+
+-- AUTOCLOSE
+vim.api.nvim_set_keymap("i", "(", "()<Left>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "[", "[]<Left>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "{", "{}<Left>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", '"', '""<Left>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "'", "''<Left>", { noremap = true, silent = true })
+
