40a1a492c10fe9fae7edcb276e765b3610396829
Guilherme Alvarez <guilherme.barboza@veredaeducacao.com>
2024-12-23 11:54:42 -0300

    refactor(mason): add new configs from old nvim project

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -12,6 +12,10 @@ return {
 		"L3MON4D3/LuaSnip",           -- Snippet engine
 		"rafamadriz/friendly-snippets", -- Snippet collection
 		"onsails/lspkind.nvim",       -- For icons in completion menu
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		{ 'j-hui/fidget.nvim', opts = {} },
+		{ 'folke/neodev.nvim', opts = {} },
+
 	},
 	config = function()
 		-- Mason setup
@@ -21,5 +25,28 @@ return {
 		require('plugins.LSP.lspkind')
 		require('plugins.LSP.cmp')
 		require('plugins.LSP.mason')
+
+		vim.api.nvim_create_autocmd('LspAttach', {
+			group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+			callback = function(event)
+				-- NOTE: Remember that lua is a real programming language, and as such it is possible
+				-- to define small helper and utility functions so you don't have to repeat yourself
+				-- many times.
+				--
+				-- When you move your cursor, the highlights will be cleared (the second autocommand).
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if client and client.server_capabilities.documentHighlightProvider then
+					vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+						buffer = event.buf,
+						callback = vim.lsp.buf.document_highlight,
+					})
+
+					vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+						buffer = event.buf,
+						callback = vim.lsp.buf.clear_references,
+					})
+				end
+			end,
+		})
 	end
 }
