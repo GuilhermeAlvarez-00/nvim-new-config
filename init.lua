66f87f88af49a1ebc1f73a62adcd80482abc24d7
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-04-20 14:12:21 -0300

    style: removing unused code

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
-
--- AUTOCLOSE
---vim.api.nvim_set_keymap("i", "(", "()<Left>", { noremap = true, silent = true })
---vim.api.nvim_set_keymap("i", "[", "[]<Left>", { noremap = true, silent = true })
---vim.api.nvim_set_keymap("i", "{", "{}<Left>", { noremap = true, silent = true })
---vim.api.nvim_set_keymap("i", '"', '""<Left>', { noremap = true, silent = true })
---vim.api.nvim_set_keymap("i", "'", "''<Left>", { noremap = true, silent = true })
