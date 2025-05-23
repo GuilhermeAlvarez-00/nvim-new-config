1494dbeaebbefbafb31c64246ab7dbaaff62c6df
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-05-23 16:53:36 -0300

    refactor(telescope): lazy loading and adjusting keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,13 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		'nvim-telescope/telescope.nvim',
+		cmd = "Telescope",
+		tag = '0.1.8',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [A]ll files' })
-			vim.keymap.set('n', 'fw', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+			vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 			vim.keymap.set('n', '<leader>b', builtin.buffers)
 			vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find)
 		end
