ae47d6e4d054f9c2e38a24f5585bad9f2b6ed0c9
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-05-23 16:54:13 -0300

    refactor(init): enable cache and disable all unused plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,27 @@
+vim.loader.enable()
+
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
+
+-- Disable unused default plugins
+local disabled_built_ins = {
+	"gzip",
+	"zip",
+	"zipPlugin",
+	"tar",
+	"tarPlugin",
+	"getscript",
+	"getscriptPlugin",
+	"vimball",
+	"vimballPlugin",
+	"2html_plugin",
+	"logiPat",
+	"rrhelper",
+	"spellfile_plugin",
+	"matchit",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+	vim.g["loaded_" .. plugin] = 1
+end
