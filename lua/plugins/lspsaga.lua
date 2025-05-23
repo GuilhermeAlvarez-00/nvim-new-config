e56f3eaf57a6fca76bd295fbf9e9c1646d8f4b03
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2025-05-23 16:52:45 -0300

    refactor(lspsaga): adjusting keymaps

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -22,8 +22,8 @@ return {
 		vim.keymap.set('n', '<leader>sd', function()
 			vim.diagnostic.open_float(nil, { border = "rounded" })
 		end, { desc = "Show line diagnostics" })
-		vim.keymap.set('n', 'K', '<CMD>Lspsaga peek_definition<CR>')
+		vim.keymap.set('n', '<leader>K', '<CMD>Lspsaga peek_definition<CR>')
 		vim.keymap.set('n', '<leader>hd', '<CMD>Lspsaga hover_doc <CR>')
-		vim.keymap.set('n', 'ca', '<CMD>Lspsaga code_action <CR>')
+		vim.keymap.set('n', '<leader>ca', '<CMD>Lspsaga code_action <CR>')
 	end,
 }
